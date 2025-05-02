7aecc45e651eae753e997cd4394625f81d06dd2a
chellenlee <y.funa@kkd.biglobe.ne.jp>
2025-05-02 23:53:19 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('qr-app-cache-v1').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon.png',
+        'https://unpkg.com/@zxing/library@latest'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
